0a18718602e539553d5b4c31d41ac03cb51b8ee9
nieshihao <2075259561@qq.com>
2020-05-06 17:35:44 +0800

    vuecli

---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main"></div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped lang="less">
+    .main {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: #7d7d7d;
+    }
+</style>
---
+++ src/pages/Vuex.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Vuex"
+    }
+</script>
+
+<style scoped>
+
+</style>
