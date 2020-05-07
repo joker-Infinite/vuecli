8a7c4b31973a09018e605174187ebd7850b57202
nieshihao <2075259561@qq.com>
2020-05-07 10:39:46 +0800

    vuecli-vuex

--- src/pages/Vuex.vue
+++ src/pages/Vuex.vue
@@ -3,11 +3,20 @@
         <el-button @click="addNumber">+</el-button>
         <el-input :value="count"></el-input>
         <el-button @click="lessNumber">-</el-button>
-        <el-button @click="getStates"></el-button>
+        <el-button @click="getStates">getters</el-button>
+        <el-button @click="newBankName">commit</el-button>
+        <br>
+        {{todoes}}****
+        {{doneTodosCounts()}}****
+        {{doneTodos}}****
+        {{doneTodosCount}}****
+        {{bankName}}
     </div>
 </template>
 
 <script>
+
+    import {mapGetters} from "vuex";
 
     export default {
         name: "Vuex",
@@ -17,9 +26,21 @@
         computed: {
             count() {
                 return this.$store.state.count;
-            }
+            },
+            todoes() {
+                return this.$store.getters.doneTodos;
+            },
+            //直接获取store中的方法所返回的参数
+            ...mapGetters([
+                'doneTodos',
+                'doneTodosCount',
+                'bankName'
+            ])
         },
         methods: {
+            doneTodosCounts() {
+                return this.$store.getters.doneTodosCount;
+            },
             addNumber() {
                 this.$store.dispatch('actionIncrease')
             },
@@ -27,11 +48,13 @@
                 this.$store.dispatch('actionSubtract')
             },
             getStates() {
-                console.log(this.$store)
                 let getState = this.$store.state;
                 let getters = this.$store.getters;
                 console.log(getState);
                 console.log(getters);
+            },
+            newBankName() {
+                this.$store.commit('newBankName', '汇丰银行');
             }
         }
     }
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,13 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        count: 0
+        count: 0,
+        todos: [
+            {id: 1, text: '水果类', done: true},
+            {id: 2, text: '苹果', done: true},
+            {id: 3, text: '荔枝', done: false}
+        ],
+        bankName: '中国银行'
     },
     mutations: {
         increase(state) {
@@ -13,14 +19,32 @@ export default new Vuex.Store({
         },
         subtract(state) {
             state.count--;
+        },
+        newBankName: (state, msg) => {
+            state.bankName = msg;
         }
     },
     actions: {
+        /*
+        * commit:同步操作，写法：this.$store.commit('mutations方法名',值)，数据提交至mutations，可用于登陆成功后读取用户信息，写到缓存里
+        * dispatch:异步操作（向后台提交数据），写法： this.$store.dispatch('mutations方法名',值),数据提交至actions，可用于向后台提交数据
+        * */
         actionIncrease({commit}) {
             commit('increase');
         },
         actionSubtract({commit}) {
             commit('subtract');
+        }
+    },
+    getters: {
+        doneTodos: state => {//通过方法访问
+            return state.todos.filter(todo => todo.done);
+        },
+        doneTodosCount: ((state, getters) => {//通过属性访问
+            return getters.doneTodos.length;
+        }),
+        bankName: state => {
+            return state.bankName;
         }
     },
     modules: {}
